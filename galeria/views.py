4864aa859eda855c3142f4214528b254c0cff58e
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2025-02-13 09:13:24 -0300

    remove static galeria folder

--- galeria/views.py
+++ galeria/views.py
@@ -3,9 +3,18 @@ from galeria.models import Fotografia
 
 # Create your views here.
 def index(request):
-    fotografias = Fotografia.objects.filter(publicada=True)
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+    if "nome" in request.GET:
+        nome_buscar = request.GET['nome']
+        if nome_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_buscar)
+
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
