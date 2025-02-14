00555e50359d55566ece980bdf0f64adff21a02a
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2025-02-14 10:50:29 -0300

    create login form.

---
+++ usuarios/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+            label="Nome de login",
+            required=True,
+            max_length=100,
+            widget=forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Ex: Matheus Passos",
+                }
+            )
+    )
+    senha = forms.CharField(
+            label="Senha",
+            required=True,
+            max_length=70,
+            widget=forms.PasswordInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "digite a sua senha.",
+                }
+            ),
+    )
