93a00cabf84d2fe1f43fa17067e90091cb87e2b1
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2025-02-14 16:50:13 -0300

    add form for registring a user

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,25 +1,78 @@
 from django import forms
 
+
 class LoginForms(forms.Form):
+    """
+    Form de login
+    """
+
     nome_login = forms.CharField(
-            label="Nome de login",
-            required=True,
-            max_length=100,
-            widget=forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Ex: Matheus Passos",
-                }
-            )
+        label="Nome de login",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex: Matheus Passos",
+            }
+        ),
     )
     senha = forms.CharField(
-            label="Senha",
-            required=True,
-            max_length=70,
-            widget=forms.PasswordInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "digite a sua senha.",
-                }
-            ),
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "digite a sua senha.",
+            }
+        ),
+    )
+
+
+class CadastroForms(forms.Form):
+    """
+    Formalario do cadastro de usuarios.
+    """
+
+    nome_cadastro = forms.CharField(
+        label="Nome de cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Ex.: Matheus Passos"}
+        ),
+    )
+    email = forms.EmailField(
+        label="E-mail",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: matheus.passos@xpto.com",
+            }
+        ),
+    )
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "digite a sua senha.",
+            }
+        ),
+    )
+    senha_2 = forms.CharField(
+        label="Confirme sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "digite a sua senha novamente.",
+            }
+        ),
     )
