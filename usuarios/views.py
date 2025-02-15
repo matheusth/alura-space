e5c86096d40242ce92a44e802d12894778e49a96
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2025-02-14 21:41:20 -0300

    added messages

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,56 @@
-from django.shortcuts import render
-from usuarios.forms import LoginForms, CadastroForms
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+
+from usuarios.forms import CadastroForms, LoginForms
+
 
 def login(request):
     form = LoginForms()
-    return render(request, 'usuarios/login.html', {'form': form})
+    if request.method == "POST":
+        form = LoginForms(request.POST)
+        if form.is_valid():
+            nome = form["nome_login"].value()
+            senha = form["senha"].value()
+            usuario = auth.authenticate(request, username=nome, password=senha)
+            if usuario is not None:
+                auth.login(request, usuario)
+                messages.success(request, f"Usuario {nome}, autenticado com sucesso!")
+                return redirect("index")
+            else:
+                messages.error(
+                    request,
+                    f"Erro ao autenticar, verifique o nome de usuario e a senha!",
+                )
+                return redirect("login")
+
+    return render(request, "usuarios/login.html", {"form": form})
+
 
 def cadastro(request):
     form = CadastroForms()
-    return render(request, 'usuarios/cadastro.html', {'form': form})
+    if request.method == "POST":
+        form = CadastroForms(request.POST)
+        if form.is_valid():
+            if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(
+                    request, "A senha e a confirmação de senha estão diferentes."
+                )
+                return redirect("cadastro")
+            nome = form["nome_cadastro"].value()
+            email = form["email"].value()
+            senha = form["senha_1"].value()
 
+            if User.objects.filter(username=nome).exists():
+                messages.error(request, "Já existe um usuário com esse nome.")
+                return redirect("cadastro")
+            usuario = User.objects.create_user(
+                username=nome, email=email, password=senha
+            )
+            usuario.save()
+            messages.success(
+                request, f"Usuario {usuario.username}, cadastrado com sucesso!"
+            )
+            return redirect("login")
 
+    return render(request, "usuarios/cadastro.html", {"form": form})
